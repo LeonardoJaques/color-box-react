f29ce4f2f658952ea11138be0cb00e20aef9ed39
LeonardoJaques <jaques.projetos@outlook.com>
2021-02-27 21:40:20 -0300

    Boxes traing, special atention in helpers.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BoxCointainer from './BoxContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <BoxCointainer />
+    </div>
+  );
+}
+
+export default App;
